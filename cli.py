40089d77af2abb0b8230c83fdec46d432a53ba4c
Andrea Ferraris <andrea.ferraris90@gmail.com>
2017-01-18 11:49:05 +0100

    update cli.py

--- cli.py
+++ cli.py
@@ -7,16 +7,11 @@ from lib.decompiler import DexFile
 
 
 from IPython.terminal.embed import InteractiveShellEmbed
-from traitlets.config import Config
 
 
 def interact():
-    cfg = Config()
-    ipshell = InteractiveShellEmbed(
-        config=cfg,
-        banner1="APK Static Analyzer version 0")
+    ipshell = InteractiveShellEmbed()
     ipshell()
-
 
 def main():
 	#print "APK Static Analyzer version 0"
